cb1975a9ed5e5e7ddc11640f7bc2c8562c2c45fe
Rudi Wagner <wagner.rudi@gmx.at>
2015-01-23 23:11:07 +0100

    Added Frostie Prefab

---
+++ Assets/Scripts/FrostieScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrostieScript : MonoBehaviour {
+
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public float speed = 10;
+
+	public float jumpHight = 5;
+	
+	// 2 - Store the movement
+	private float movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		
+		// 4 - Movement per direction
+		movement =speed * inputX;
+
+		if (Input.GetKeyDown ("space")){
+			rigidbody2D.AddForce(new Vector2(0, jumpHight), ForceMode2D.Impulse);
+		} 
+		
+		// 6 - Make sure we are not outside the camera bounds
+		var dist = (transform.position - Camera.main.transform.position).z;
+		
+		var leftBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).x;
+		
+		var rightBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(1, 0, dist)
+			).x;
+		
+		var topBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)
+			).y;
+		
+		var bottomBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 1, dist)
+			).y;
+		
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+			transform.position.z
+			);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		float speedX = movement;
+		float speedY = rigidbody2D.velocity.y;
+		rigidbody2D.velocity = new Vector2(speedX,speedY);
+	}
+}
