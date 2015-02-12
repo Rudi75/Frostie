7a890f9c422d67ac24d1da46613e299a10998361
Caterina Nahler <caterina.nahler@student.tugraz.at>
2015-02-12 18:09:19 +0100

    some changes befor merge

--- Assets/Scripts/FrostieScript.cs
+++ Assets/Scripts/FrostieScript.cs
@@ -91,21 +91,20 @@ public class FrostieScript : MonoBehaviour {
 		var rightBorder = Camera.main.ViewportToWorldPoint(
 			new Vector3(1, 0, dist)
 			).x;
-		
-		var topBorder = Camera.main.ViewportToWorldPoint(
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(
 			new Vector3(0, 0, dist)
 			).y;
-		
-		var bottomBorder = Camera.main.ViewportToWorldPoint(
+
+        var topBorder = Camera.main.ViewportToWorldPoint(
 			new Vector3(0, 1, dist)
 			).y;
 		
 		transform.position = new Vector3(
 			Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
-			Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+            Mathf.Clamp(transform.position.y, transform.position.y, topBorder),//bottomBorder
 			transform.position.z
 			);
-		
 	}
 	
 	void FixedUpdate()
