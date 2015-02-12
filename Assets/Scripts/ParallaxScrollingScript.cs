14ea0b38b811d42243a5675548109bfb2982d65c
davidkastl <kastl.david@gmail.com>
2015-02-12 13:58:02 +0100

    unnecessary code removed

--- Assets/Scripts/ParallaxScrollingScript.cs
+++ Assets/Scripts/ParallaxScrollingScript.cs
@@ -6,24 +6,14 @@ public class ParallaxScrollingScript : MonoBehaviour
   public Vector2 speed = new Vector2(1, 1);
   private Vector2 direction = new Vector2(0, 0);
 
-  public Transform Player;
   public Transform Camera;
-  public Vector2 Margin;
-  public Vector2 Smoothing;
-
-  public BoxCollider2D Bounds;
 
   private Vector3 minPosition, maxPosition;
   private float x,y;
 
-  public bool IsFollowing = true;
-
 	// Use this for initialization
 	void Start ()
   {
-    minPosition = Bounds.bounds.min;
-    maxPosition = Bounds.bounds.max;
-
     x = Camera.position.x;
     y = Camera.position.y;
 	}
@@ -64,12 +54,10 @@ public class ParallaxScrollingScript : MonoBehaviour
       direction.y = 0;
     }
 
-    
     Vector3 movement = new Vector3(
       speed.x * direction.x,
       speed.y * direction.y,
       0);
-
 
     movement *= Time.deltaTime;
     transform.Translate(movement);
