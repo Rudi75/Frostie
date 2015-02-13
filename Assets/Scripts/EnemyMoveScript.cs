2f5180cb2493c7b0c81e791bd1ac4ea38037b0bd
Artur Knaus <artur.knaus@student.tugraz.at>
2015-02-13 20:54:48 +0100

    hedgehog base functionality: run, shoot, and kill frosty (in multiple ways ;) )

---
+++ Assets/Scripts/EnemyMoveScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(5, 5);
+    public Vector2 direction = new Vector2(-1, 0);
+
+    private Vector2 movement;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // 2 - Movement
+        movement = new Vector2(
+            speed.x * direction.x,
+            speed.y * direction.y);
+    }
+
+    void FixedUpdate()
+    {        
+        rigidbody2D.velocity = movement;
+    }
+}
