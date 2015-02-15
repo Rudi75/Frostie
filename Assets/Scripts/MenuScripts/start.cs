a79d96f2ba6ec5545a19bd3a3421b7b6b0548b10
davidkastl <kastl.david@gmail.com>
2015-02-15 22:37:11 +0100

    First steps to a menu, clicking on start opens a scene

---
+++ Assets/Scripts/MenuScripts/start.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start : MonoBehaviour {
+
+  public string level = "";
+
+	// Use this for initialization
+	void Start ()
+  {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+	
+	}
+
+  void OnMouseDown()
+  {
+    Application.LoadLevel(level);
+  }
+}
