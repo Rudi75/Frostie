38c2231755d37f16f393bacc6f989c673d505f9d
Rudi Wagner <wagner.rudi@gmx.at>
2015-03-03 19:55:56 +0100

    savePoint Added

---
+++ Assets/Scripts/SavePointScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SavePointScript : MonoBehaviour {
+   
+    private bool activated = false;
+    private Transform fire;
+
+    public void Awake()
+    {
+        foreach (Transform childTransform in transform)
+        {
+            if(childTransform.name.Contains("Fire"))
+            {
+                fire = childTransform;
+            }
+        }
+    }
+public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("servas");
+        if (other.tag.Contains("Player"))
+        {
+            activated = true;
+            fire.gameObject.SetActive(true);
+            FrostiePartManager.spawnPosition = transform.position;
+        }
+    }
+}
