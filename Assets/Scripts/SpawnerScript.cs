e72fa74f766ad7625573f43ae2517b1863f7334e
Caterina Nahler <caterina.nahler@student.tugraz.at>
2015-03-03 20:41:05 +0100

    worked on level_1 and level_3

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -6,10 +6,6 @@ public class SpawnerScript : TargetActionScript
     public bool OnAction = false;
     public GameObject objectToSpawnPrefab;
     private GameObject thingy;
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
