77421381f0484457566ca33a9184e5533bcf1939
Rudi Wagner <wagner.rudi@gmx.at>
2015-03-07 20:48:31 +0100

    HeadCloneScript modified

--- Assets/Scripts/HeadCloneScript.cs
+++ Assets/Scripts/HeadCloneScript.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using AssemblyCSharp;
 
 public class HeadCloneScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(rigidbody2D.velocity.Equals(Vector2.zero))
+
+        GameObject collidingObject = CustomCollisionHelper.getCollidingObject(collider2D, Assets.Scripts.Utils.Enums.Edges.BOTTOM, 0.1f);
+        if(rigidbody2D.velocity.Equals(Vector2.zero) && collidingObject != null)
         {
             FrostiePartManager frostiePartManager = transform.parent.GetComponentInChildren<FrostiePartManager>();
             frostiePartManager.recallHead();
